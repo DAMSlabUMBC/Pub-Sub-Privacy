9141853604cb8b7d6824432c6d3f497a535cb4ec
Leon Tang <130707295+tangleon8@users.noreply.github.com>
2025-03-06 21:54:47 -0500

    Update GlobalDefs.py

--- benchmark/GlobalDefs.py
+++ benchmark/GlobalDefs.py
@@ -1,6 +1,6 @@
 from enum import Enum, IntEnum
 from types import ModuleType
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List  # Import List for compatibility
 
 # These provide type checking without cyclic imports
 if TYPE_CHECKING:
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
 # Framework Method Enums
 class PurposeManagementMethod(Enum):
     PM_0 = "None"
-    PM_1 = "Purpose-Enconding Topics"
+    PM_1 = "Purpose-Encoding Topics"
     PM_2 = "Per-Message Declaration"
     PM_3 = "Registration by Message"
     PM_4 = "Registration by Topic"
@@ -23,25 +23,30 @@ class C1RightsMethod(Enum):
 class C2RightsMethod(Enum):
     C2_0 = "None"
     C2_1 = "Direct Publication"
-    C2_2 = "Broker-Facilitatied"
+    C2_2 = "Broker-Facilitated"
 
 class C3RightsMethod(Enum):
     C3_0 = "None"
     C3_1 = "Direct Publication"
-    C3_2 = "Broker-Facilitatied"
+    C3_2 = "Broker-Facilitated"
 
 # Exit Code definitions
 class ExitCode(IntEnum):
-    SUCCESS = 0,
-    BAD_ARGUMENT = 1,
-    MALFORMED_CONFIG = 2,
-    BAD_CLIENT_API = 3,
-    FAILED_TO_INIT_SYNC = 4,
-    FAILED_TO_INIT_LOGGING = 5,
+    SUCCESS = 0
+    BAD_ARGUMENT = 1
+    MALFORMED_CONFIG = 2
+    BAD_CLIENT_API = 3
+    FAILED_TO_INIT_SYNC = 4
+    FAILED_TO_INIT_LOGGING = 5
     UNKNOWN_ERROR = 99
 
 CLIENT_MODULE: ModuleType
 SYNC_MODULE: 'BenchmarkSynchronizer'
 LOGGING_MODULE: 'ResultLogger'
-CLIENT_FUNCTIONS: list[str] = ["create_v5_client", "connect_client", "subscribe_with_purpose_filter", 
-                               "register_publish_purpose_for_topic", "publish_with_purpose"]
+CLIENT_FUNCTIONS: List[str] = [  
+    "create_v5_client", 
+    "connect_client", 
+    "subscribe_with_purpose_filter",
+    "register_publish_purpose_for_topic", 
+    "publish_with_purpose"
+]
